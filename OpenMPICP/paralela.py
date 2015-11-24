cb591f1cc583430e200b2700f19ee52d07c31ec5
paulo <paulo@paulo-MS-7788>
2015-11-24 19:57:10 -0300

    Solução paralela terminado.

--- OpenMPICP/paralela.py
+++ OpenMPICP/paralela.py
@@ -4,20 +4,31 @@ from mpi4py import MPI
 from fibonacci import fibonacci
 
 comm = MPI.COMM_WORLD
-rank=comm.rank
-size=comm.size
-name=MPI.Get_processor_name()
+size = comm.Get_size()
+rank = comm.Get_rank()
 
-numero = 4
-total = 0
+numero = 40
+lista_sequencia = []
+soma = 0
 
 if rank == 0:
-    comm.send(1, dest=1, tag=1)
-    total = total + comm.recv(source=1, tag=4)
+    for i in range(1, numero+1):
+        lista_sequencia.append(i)
+    data = lista_sequencia
+    print 'Lista que será processada:',data
+else:
+    data = None
+   
+data = comm.scatter(data, root=0)
+res = fibonacci(data)
+data = res
+print 'Processo',rank,'tem dado:',data
 
-for i in range(1, numero):
-    if rank == i:
-        receive = comm.recv(source=i-1, tag=i)
-        res = fibonacci(receive)
-        comm.send(res, dest=0, tag=i)
-print total
+newData = comm.gather(data,root=0)
+
+if rank == 0:
+    print 'Processo mestre dado:',newData
+    print "Soma total :", sum(newData)
+
+
+
--- OpenMPICP/sequencial.py
+++ OpenMPICP/sequencial.py
@@ -1,10 +1,20 @@
 from fibonacci import fibonacci
 numero = 40
 res = 0
+lista_sequencia = []
+lista_sequencia_fib = []
 
-for i in range(1, numero):
-    fibonacci_numero = fibonacci(i)
-    print " %d " %fibonacci_numero
-    res = res + fibonacci_numero
+for i in range(1, numero+1):
+    lista_sequencia.append(i)
+print "Sequencia a ser processada: " + str(lista_sequencia)
+
+indice = 0
+
+for i in lista_sequencia:
+    lista_sequencia_fib.append(fibonacci(i))
+    print " %d " %lista_sequencia_fib[indice]
+    indice += 1
+
+print "Lista fibonacci: ", lista_sequencia_fib
     
-print "Soma total: %d" %res
+print "Soma total: ", sum(lista_sequencia_fib)
