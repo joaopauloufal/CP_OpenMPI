791ed6ed34bfc1a56d32cc95ecedad599f13a191
lapec <lapec@lapec-HP-EliteDesk-800-G1-SFF>
2015-11-27 17:08:22 -0300

    Resolvido problema na quantidde de processos.

--- OpenMPICP/paralela.py
+++ OpenMPICP/paralela.py
@@ -15,23 +15,35 @@ lista_sequencia = []
 soma = 0
 
 if rank == 0:
+    
     for i in range(1, numero+1):
         lista_sequencia.append(i)
     data = lista_sequencia
+    
+    chunks = [[] for i in range(size)]
+    
+    for i, chunk in enumerate(data):
+        chunks[i % size].append(chunk)
     print 'Lista que será processada:',data
 else:
     data = None
+    chunks = None
    
-data = comm.scatter(data, root=0)
-res = fibonacci(data)
-data = res
-print 'Processo',rank,'tem dado:',data
+data = comm.scatter(chunks, root=0)
+
+for i in range(len(data)):
+    if type(data) == list:
+        res = fibonacci(data[i])
+    else:
+        res = fibonacci(data)
+    data = res
+    print 'Processo',rank,'tem dado:',res
 
 newData = comm.gather(data,root=0)
 
 if rank == 0:
     print 'Processo mestre dado:',newData
-    print "Soma total :", sum(newData)
+    #print "Soma total :", sum(newData)
 
     fim = time.time()
     print "Tempo total de execução: ", fim-ini
